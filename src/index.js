ea5704eac1d6b2373ccc9b2663f33be93ce82917
Emilio Scodeler <emilio.scodeler@solides.com.br>
2022-11-11 16:40:54 -0300

    apply styled components

--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
-import "./index.css";
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
+import styled, { createGlobalStyle } from "styled-components";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+  }
+
+  code {
+    font-family: monospace;
+  }
+
+  li {
+    list-style: none;
+  }
+`;
+
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(90deg, #00876e 35%, #03bf9a);
+`;
+
 const App = () => {
   return (
-    <div className="app">
+    <AppContainer>
       <Header />
-    </div>
+    </AppContainer>
   );
 };
 
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
